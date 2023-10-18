6f1ea671116cb90f1b1aa625c2fa44bdb48f8b01
Brad <acceleratorzxc@gmail.com>
2023-10-18 03:03:08 -0500

    hice reaidad mi capricho2

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
    base: './',
+   define: { 'process.env': {} },
+   resolve: {
+      alias: {
+         '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      extensions: [
+         '.js',
+         '.json',
+         '.jsx',
+         '.mjs',
+         '.ts',
+         '.tsx',
+      ],
+   },
 });
